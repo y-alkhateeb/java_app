d9cc8aba126bb8ae40ce114ecb0d6988f01709ff
Yousef <yuos-_-1y@live.it>
2020-12-10 00:34:48 +0200

    init

---
+++ app/src/main/java/com/example/baitiwb303_hw_f20_c/activity/LoginActivity.java
@@ -0,0 +1,75 @@
+package com.example.baitiwb303_hw_f20_c.activity;
+
+import android.content.Intent;
+import android.util.Patterns;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
+import android.widget.EditText;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import com.example.baitiwb303_hw_f20_c.R;
+import com.google.android.material.textfield.TextInputLayout;
+
+public class LoginActivity extends AppCompatActivity {
+
+    EditText username, password;
+    Button login;
+    boolean isEmailValid, isPasswordValid;
+    TextInputLayout emailError, passError;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login);
+
+        username = (EditText) findViewById(R.id.username);
+        password = (EditText) findViewById(R.id.password);
+        login = (Button) findViewById(R.id.login);
+        emailError = (TextInputLayout) findViewById(R.id.emailError);
+        passError = (TextInputLayout) findViewById(R.id.passError);
+
+        login.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                SetValidation();
+            }
+        });
+
+
+    }
+
+    public void SetValidation() {
+        // Check for a valid email address.
+        if (username.getText().toString().isEmpty()) {
+            emailError.setError(getResources().getString(R.string.username_error));
+            isEmailValid = false;
+        } else  {
+            isEmailValid = true;
+            emailError.setErrorEnabled(false);
+        }
+
+        // Check for a valid password.
+        if (password.getText().toString().isEmpty()) {
+            passError.setError(getResources().getString(R.string.password_error));
+            isPasswordValid = false;
+        } else if (password.getText().length() < 6) {
+            passError.setError(getResources().getString(R.string.error_invalid_password));
+            isPasswordValid = false;
+        } else  {
+            isPasswordValid = true;
+            passError.setErrorEnabled(false);
+        }
+
+        if (isEmailValid && isPasswordValid) {
+            Toast.makeText(getApplicationContext(), "Successfully", Toast.LENGTH_SHORT).show();
+            Intent intent = new Intent(this, MainActivity.class);
+            this.finish();
+            startActivity(intent);
+        }
+
+    }
+
+}
---
+++ app/src/main/java/com/example/baitiwb303_hw_f20_c/activity/ui/home/HomeViewModel.java
@@ -0,0 +1,19 @@
+package com.example.baitiwb303_hw_f20_c.activity.ui.home;
+
+import androidx.lifecycle.LiveData;
+import androidx.lifecycle.MutableLiveData;
+import androidx.lifecycle.ViewModel;
+
+public class HomeViewModel extends ViewModel {
+
+    private MutableLiveData<String> mText;
+
+    public HomeViewModel() {
+        mText = new MutableLiveData<>();
+        mText.setValue("This is home fragment");
+    }
+
+    public LiveData<String> getText() {
+        return mText;
+    }
+}
